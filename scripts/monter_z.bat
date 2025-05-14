cad812a9ac121d67f09e7eff2342ec9df9c1b6bc
manastria <manastria@users.noreply.github.com>
2025-05-14 18:54:50 +0200

    Update password in monter_z.bat for network share access

--- scripts/monter_z.bat
+++ scripts/monter_z.bat
@@ -2,7 +2,7 @@
 set IP=192.168.56.101
 set PARTAGE=web
 set UTILISATEUR=etudiant
-set MDP=etudiant123
+set MDP=netlab123
 
 net use Z: /delete >nul 2>&1
 net use Z: \\%IP%\%PARTAGE% /user:%UTILISATEUR% %MDP%
