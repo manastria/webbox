4cce4e29f6a805df901b7040d0014bd8055344ff
manastria <manastria@users.noreply.github.com>
2025-05-18 19:50:44 +0200

    Update SHARE_NAME variable and add yadm installation and repository cloning steps in setup script

--- scripts/install/setup-lxde.sh
+++ scripts/install/setup-lxde.sh
@@ -6,7 +6,7 @@ set -e
 USERNAME="etudiant"
 USER_HOME="/home/$USERNAME"
 IP_SITE="192.168.56.50"
-SHARE_NAME="www"
+SHARE_NAME="web"
 
 # sudo apt update && sudo apt upgrade -y
 
@@ -92,6 +92,34 @@ grep -qxF "@firefox-esr http://$IP_SITE" "$AUTOSTART_FILE" || \
 
 # 4) Droits
 sudo chown -R "$USERNAME:$USERNAME" "$USER_HOME/.config"
+
+# --- 7. Cloner le dépôt WebBox + Dotfiles avec yadm
+echo "[+] Clonage du dépôt WebBox et configuration avec yadm"
+
+# 1) Installer yadm s'il n'est pas présent
+if ! command -v yadm &> /dev/null; then
+    echo "[+] Installation de yadm"
+    sudo apt install -y yadm
+fi
+
+# 2) Cloner le dépôt WebBox dans le home de l'utilisateur
+if [ ! -d "$USER_HOME/webbox" ]; then
+    sudo -u "$USERNAME" git clone https://github.com/manastria/webbox.git "$USER_HOME/webbox"
+else
+    echo "[!] Le dossier webbox existe déjà dans $USER_HOME"
+fi
+
+# 3) Cloner et appliquer les dotfiles avec yadm
+sudo -u "$USERNAME" yadm clone https://github.com/manastria/dotfile.git || {
+    echo "[!] Échec du clone YADM, tentative de suppression du dépôt existant..."
+    sudo -u "$USERNAME" rm -rf "$USER_HOME/.local/share/yadm/repo.git"
+    sudo -u "$USERNAME" yadm clone https://github.com/manastria/dotfile.git
+}
+
+# 4) Forcer l'application des dotfiles depuis la branche master
+sudo -u "$USERNAME" yadm reset --hard master
+
+echo "[✔] Dépôts clonés et dotfiles appliqués pour $USERNAME"
 
 
 echo "[✔] Installation et configuration terminées pour l'utilisateur $USERNAME"
